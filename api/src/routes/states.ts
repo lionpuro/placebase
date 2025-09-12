f7964bd30d181c2f22be84bb88c3d44abdc52bee
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-09-12 16:03:16 +0300

    add state details endpoint

--- api/src/routes/states.ts
+++ api/src/routes/states.ts
@@ -4,6 +4,7 @@ import { StateSchema } from "../schemas/index.js";
 import {
 	CountryCodeSchema,
 	CountryStatesQuerySchema,
+	StateCodeSchema,
 	StatesQuerySchema,
 } from "../schemas/request.js";
 
@@ -58,6 +59,38 @@ export default (async function (app) {
 				return reply.code(200).send(states);
 			} catch (err) {
 				console.log(err);
+				return reply.code(500).send({ message: "Internal server error" });
+			}
+		},
+	});
+	app.route({
+		method: "GET",
+		url: "/countries/:country_code/states/:state_code",
+		schema: {
+			summary: "State details",
+			description: "Find state details by its ISO2 code",
+			tags: ["States"],
+			params: Type.Object({
+				country_code: CountryCodeSchema,
+				state_code: StateCodeSchema,
+			}),
+			response: {
+				200: StateSchema,
+				404: Type.Object({ message: Type.String() }),
+				500: Type.Object({ message: Type.String() }),
+			},
+		},
+		handler: async (req, reply) => {
+			try {
+				const [state] = await app.stateRepository.find({
+					country: req.params.country_code,
+					iso_code: req.params.state_code,
+				});
+				if (!state) {
+					return reply.code(404).send({ message: "Not found" });
+				}
+				return reply.code(200).send(state);
+			} catch (err) {
 				return reply.code(500).send({ message: "Internal server error" });
 			}
 		},

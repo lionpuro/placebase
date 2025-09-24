e1859b6056c2266ad1b443452249b9aee3661038
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-09-24 09:07:44 +0300

    fix pg timestamp parsing to ISO8601 in UTC

--- api/src/plugins/external/postgres.ts
+++ api/src/plugins/external/postgres.ts
@@ -1,6 +1,12 @@
 import { type FastifyInstance } from "fastify";
+import PG from "pg";
 import postgres from "@fastify/postgres";
 import fp from "fastify-plugin";
+
+// timestamp without time zone
+PG.types.setTypeParser(1114, function (str) {
+	return new Date(str + "+0000");
+});
 
 const config = () => {
 	const {
@@ -11,10 +17,11 @@ const config = () => {
 		POSTGRES_DB,
 	} = process.env;
 	return {
+		pg: PG,
 		connectionString: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable`,
 	};
 };
 
 export default fp(async (fastify: FastifyInstance, opts) => {
-	fastify.register(postgres, config());
+	await fastify.register(postgres, config());
 });

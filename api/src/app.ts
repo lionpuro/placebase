5ecded111768daaf5219e4cc4b159a5e37d26881
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-09-27 11:04:41 +0300

    register auth hook in autohooks file

--- api/src/app.ts
+++ api/src/app.ts
@@ -22,6 +22,8 @@ export async function createApp(opts: Partial<typeof defaultOptions> = {}) {
 	await app.register(autoload, {
 		dir: path.join(import.meta.dirname, "routes"),
 		ignorePattern: /^.*(?:test|spec).(js|ts)$/,
+		autoHooks: true,
+		cascadeHooks: true,
 	});
 
 	return app;
--- api/src/plugins/app/api-auth.ts
+++ api/src/plugins/app/api-auth.ts
@@ -41,28 +41,4 @@ export function createPlugin(app: FastifyInstance): APIAuth {
 
 export default fp((app) => {
 	app.decorate("apiAuth", createPlugin(app));
-	app.addHook("onRequest", async (request, reply) => {
-		if (
-			!request.url.startsWith("/internal") &&
-			!request.url.endsWith("openapi.json")
-		) {
-			const header = request.headers["x-api-key"];
-			if (!header) {
-				return reply.code(401).send({ message: "Missing API key" });
-			}
-			const key = typeof header === "string" ? header : header[0];
-			if (!key) {
-				return reply.code(401).send({ message: "Missing API key" });
-			}
-			try {
-				const valid = await app.apiAuth.verifyAPIKey(key);
-				if (!valid) {
-					return reply.code(401).send({ message: "Invalid API key" });
-				}
-			} catch (err) {
-				app.log.error(err);
-				return reply.code(500).send({ message: "Failed to verify API key" });
-			}
-		}
-	});
 });

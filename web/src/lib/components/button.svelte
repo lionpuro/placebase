f9ca16d8bebffc32e202bfd5a9589d09df09d43e
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-10-01 10:16:26 +0300

    style disabled button

--- web/src/lib/components/button.svelte
+++ web/src/lib/components/button.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import type { HTMLButtonAttributes } from "svelte/elements";
-
+	const disabledClass =
+		" disabled:bg-base-100/80 disabled:text-base-400 disabled:cursor-not-allowed";
 	const config = {
 		variants: {
-			primary: "bg-primary-600/90 hover:bg-primary-600 text-base-white",
-			secondary: "bg-base-100/90 hover:bg-base-200/70 text-base-800",
-			outline: "hover:bg-base-50 outline outline-base-100",
-			black: "bg-base-950 hover:bg-base-950/95 text-base-white",
+			primary: "bg-primary-600/90 hover:bg-primary-600 text-base-white" + disabledClass,
+			secondary: "bg-base-100/90 hover:bg-base-200/70 text-base-800" + disabledClass,
+			outline: "hover:bg-base-50 outline outline-base-100" + disabledClass,
+			black: "bg-base-950 hover:bg-base-950/95 text-base-white" + disabledClass,
 		},
 		sizes: {
 			sm: "px-4 py-1.5 text-sm font-medium",

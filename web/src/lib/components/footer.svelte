7e70a2dd5a577fbabba4b3be8f29bf03afa6af6b
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-10-25 14:36:19 +0300

    update footer

--- web/src/lib/components/footer.svelte
+++ web/src/lib/components/footer.svelte
@@ -3,17 +3,19 @@
 	import { IconGithub } from "$lib/icons";
 </script>
 
-<footer class="mx-auto mt-16 flex w-full max-w-screen-xl px-6 py-2 sm:px-8">
-	<div class="flex w-full items-center text-sm text-base-500">
-		<Link href="/">placebase.xyz</Link>
-		<Link
-			href="https://github.com/lionpuro/placebase"
-			target="_blank"
-			rel="noopener noreferrer"
-			title="Github"
-			class="ml-auto"
-		>
-			<IconGithub />
-		</Link>
+<footer class="mx-auto mt-16 w-full max-w-screen-xl px-6 sm:px-8">
+	<div class="flex flex-col gap-2 border-t border-base-100 py-4">
+		<div class="flex items-center text-sm text-base-500">
+			<Link href="/">placebase.xyz</Link>
+			<Link
+				href="https://github.com/lionpuro/placebase"
+				target="_blank"
+				rel="noopener noreferrer"
+				title="Github"
+				class="ml-auto"
+			>
+				<IconGithub />
+			</Link>
+		</div>
 	</div>
 </footer>
